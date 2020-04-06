7ed66ba8742a06d90b8635c54ccc118021a43416
Waldo <waldo.depree@ziggo.nl>
2020-04-06 20:43:42 +0200

    Merge branch 'master' of https://github.com/waldopree/Hudo
     Conflicts:
    	main.py

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ stop = False
 while throw < 3 and stop == False:
     throw += 1
     dices = randomgen.random_cal()
-    print(dices)
+    result = recq.recog(dices)
+    if result is None:
+        print(str(dices))
+    else:
+        print(str(dices) + "\n " + result)
 
     if throw < 3:
         again = input("Again (y) or stop (n): ")
         if again == "n":
             stop = True
             print("Good job, you got: " + str(dices))
+
+
--- recq.py
+++ recq.py
@@ -9,6 +9,9 @@ def recog(list):
     six = list.count(6)
 
     list_amount = [ones, twos, threes, fours, fives, six]
+    sorted_list = sorted(list)
+    big_straight_one = [1, 2, 3, 4, 5]
+    big_straight_two = [2, 3, 4, 5, 6]
 
     if ones == 5 or twos == 5 or threes == 5 or fours == 5 or fives == 5 or six == 5:
         yahtzee = "yahtzee"
@@ -16,17 +19,21 @@ def recog(list):
     elif ones == 4 or twos == 4 or threes == 4 or fours == 4 or fives == 4 or six == 4:
         fourkind = "four of a kind"
         return fourkind
-    elif 2 in list_amount:
-        if 3 in list_amount:
-            fullhouse = "fullhouse"
-            return fullhouse
+    elif 2 and 3 in list_amount:
+        fullhouse = "fullhouse"
+        return fullhouse
     elif ones == 3 or twos == 3 or threes == 3 or fours == 3 or fives == 3 or six == 3:
         threekind = "three of a kind"
         return threekind
-
-
-# dices = randomgen.random_cal()
-test_list = [2,2,1,3,4]
-print(test_list)
-result = recog(test_list)
-print(result)
+    elif sorted_list == big_straight_one or sorted_list == big_straight_two:
+        bigstraight = "big straight!"
+        return bigstraight
+    elif 1 in sorted_list and 2 in sorted_list and 3 in sorted_list and 4 in sorted_list:
+        small_straight = "small straight!"
+        return small_straight
+    elif 2 in sorted_list and 3 in sorted_list and 4 in sorted_list and 5 in sorted_list:
+        small_straight = "small straight!!"
+        return small_straight
+    elif 3 in sorted_list and 4 in sorted_list and 5 in sorted_list and 6 in sorted_list:
+        small_straight = "small straight!!!"
+        return small_straight
